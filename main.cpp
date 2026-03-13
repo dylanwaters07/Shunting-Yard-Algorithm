7313576879c56bd48c6f1f411c7806acf4d18a91
DylanW007 <453480@bsd48.org>
2026-03-13 14:26:00 -0700

    Rough outline for Shunting Yard Algorithm
    Initial implementation of stack and queue functions.

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+BUILD A STACK USING A LINKED LIST. You don't need a separate class for this, but you DO need the push(), pop(), and peek() functions.
+
+BUILD A QUEUE USING A LINKED LIST. You don't need a separate class for this, but you DO need the enqueue() and dequeue() functions.
+
+Both of these data structures are useful for the Shunting Yard algorithm.
+
+You might need a similar stack for building your tree, too.
+
+BUILD A BINARY TREE NODE CLASS WITH A LEFT AND RIGHT POINTER. 
+
+(This is very similar to the Node class for linked list, except 
+it has a data value, left pointer, and right pointer. 
+If you want to combine the linked list node into the 
+binary tree node, meaning it has a left, right, AND next, you may.)
+
+*/
+
+void stack(){
+
+}
+
+void queue(){
+    
+}
+
+int main()
+{
+    cout << "Hello World!";
+    return 0;
+}
