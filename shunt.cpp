84e493fac99b19b1815bb6a097e525e2b19b63bf
dylanwaters07 <453480@bsd48.org>
2026-03-18 12:05:26 -0700

    Add functions

--- shunt.cpp
+++ shunt.cpp
@@ -1,11 +1,21 @@
-the actual code for stack functions
-the actual code for queue functions
-the actual code for tree node functions (if needed)
+#include "shunt.h"
 
-void stack(){
-
+void shunt::push(){
+    
 }
 
-void queue(){
+void shunt::pop(){
+    
+}
+
+void shunt::peek(){
+    
+}
+
+void shunt::enqueue(){
+    
+}
+
+void shunt::dequeue(){
     
 }
