e0b5b30e2432eb29a008e8e956a246208ffc426e
dylanwaters07 <453480@bsd48.org>
2026-03-18 12:05:51 -0700

    start class

--- shunt.h
+++ shunt.h
@@ -1,1 +1,18 @@
+#ifndef shunt_h
+#define shunt_h
 
+class shunt{
+    
+    public:
+     //
+     void push();
+     void pop();
+     void peek();
+     
+     void enqueue();
+     void dequeue();
+    private:
+     //
+};
+
+#endif
