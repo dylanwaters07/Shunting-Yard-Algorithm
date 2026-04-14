ce539550dc3267be6dda40c94ed967ffe75ea5cf
DylanW007 <453480@bsd48.org>
2026-04-13 21:22:25 -0700

    Clean up main.cpp and make it build

--- main.cpp
+++ main.cpp
@@ -3,43 +3,40 @@ Dylan Waters
 3/16/26
 Shunting Yard Algorithm
 
-Description : .
+Description : 
+
+    BUILD A STACK USING A LINKED LIST. You don't need a separate class for this, but you DO need the push(), pop(), and peek() functions.
+
+    BUILD A QUEUE USING A LINKED LIST. You don't need a separate class for this, but you DO need the enqueue() and dequeue() functions.
+
+    Both of these data structures are useful for the Shunting Yard algorithm.
+
+    You might need a similar stack for building your tree, too.
+
+    BUILD A BINARY TREE NODE CLASS WITH A LEFT AND RIGHT POINTER. 
+
+    (This is very similar to the Node class for linked list, except 
+        it has a data value, left pointer, and right pointer. 
+        If you want to combine the linked list node into the 
+        binary tree node, meaning it has a left, right, AND next, you may.)
 
 */
 
 #include <iostream>
 #include <string>
-#include "queue.h"
-#include "stack.h"
+
 using namespace std;
 
-/*
-BUILD A STACK USING A LINKED LIST. You don't need a separate class for this, but you DO need the push(), pop(), and peek() functions.
 
-BUILD A QUEUE USING A LINKED LIST. You don't need a separate class for this, but you DO need the enqueue() and dequeue() functions.
-
-Both of these data structures are useful for the Shunting Yard algorithm.
-
-You might need a similar stack for building your tree, too.
-
-BUILD A BINARY TREE NODE CLASS WITH A LEFT AND RIGHT POINTER. 
-
-(This is very similar to the Node class for linked list, except 
-it has a data value, left pointer, and right pointer. 
-If you want to combine the linked list node into the 
-binary tree node, meaning it has a left, right, AND next, you may.)
-
-*/
-
-void infix(expression){
+void infix(string expression){
     
 }
 
-void prefix(expression){
+void prefix(string expression){
     
 }
 
-void postfix(expression){
+void postfix(string expression){
     
 }
 
