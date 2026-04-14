42f34048961297b75aa5aecb176fd6d6d7f2e612
DylanW007 <453480@bsd48.org>
2026-04-13 21:35:10 -0700

    Add basic node class

---
+++ node.h
@@ -0,0 +1,21 @@
+/*
+Dylan Waters
+3/16/26
+
+*/
+
+#ifndef NODE_H
+#define NODE_H
+
+/* Node class for linked list or binary tree */
+class Node{
+    public:
+     //
+     Node* next;
+     char data;
+     
+     Node(char value){
+         data = value;
+         next = nullptr;
+     }
+};
