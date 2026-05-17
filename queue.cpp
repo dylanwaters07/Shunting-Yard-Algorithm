4e6d9179aa0c938870031b864de5d77711cd4b43
DylanW007 <453480@bsd48.org>
2026-05-17 16:14:13 -0700

    Add implementation for queue class.

--- queue.cpp
+++ queue.cpp
@@ -2,18 +2,44 @@
 Dylan Waters
 4/13/26
 
+    Queue class for managing a queue of nodes. This will be used for the Shunting Yard algorithm. It has an enqueue
+    function for adding to the back of the queue, and a dequeue function for removing from the front of the queue.
+    You can implement this using a linked list, with a front and back pointer.
+
 */
 
 #include "queue.h"
 #include "node.h"
+
 using namespace std;
 
-// Moved from shunt.cpp
-
-void Queue::enqueue(){
-    
+// enqueue adds a new node with the given character to the back of the queue
+void Queue::enqueue(char c) {
+    Node* newNode = new Node(c);
+    if (back == nullptr) {
+        front = back = newNode;
+    } else {
+        back->setNext(newNode);
+        back = newNode;
+    }
 }
 
-void Queue::dequeue(){
-    
+// dequeue removes the node at the front of the queue and deletes it
+void Queue::dequeue() {
+    if (front != nullptr) {
+        Node* temp = front;
+        front = front->getNext();
+        if (front == nullptr) {
+            back = nullptr;
+        }
+        delete temp;
+    }
+}
+
+// front returns the character at the front of the queue without removing it
+char Queue::front() {
+    if (front != nullptr) {
+        return front->getData();
+    }
+    return '\0'; // Return null character if queue is empty
 }
--- queue.h
+++ queue.h
@@ -2,17 +2,44 @@
 Dylan Waters
 4/13/26
 
+Queue class for managing a queue of nodes. This will be used for the Shunting Yard algorithm. It has an enqueue
+function for adding to the back of the queue, and a dequeue function for removing from the front of the queue.
+You can implement this using a linked list, with a front and back pointer.
+
 */
 
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include "node.h"
+
 /* Node class for linked list or binary tree */
 class Queue{
     public:
-     void enqueue();
-     void dequeue();
+        Queue(){
+            front = nullptr;
+            back = nullptr;
+        }
 
+        void enqueue(char c);
+
+        void dequeue();
+
+        char front();
+
+        bool empty() {
+            return front == nullptr;
+        }
+
+        char front();
+
+        bool empty() {
+            return front == nullptr;
+        }
+
+    private:
+        Node* front;
+        Node* back;
 };
 
 #endif
